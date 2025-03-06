8f9d746f9b6ea998e18a90d10fdc17a97e2cccdd
Weihon79 <ervan.chin@gmail.com>
2025-03-06 17:00:00 +0100

    slider changes ordre décroissant et unique keys updated

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -4,35 +4,51 @@ import { getMonth } from "../../helpers/Date";
 
 import "./style.scss";
 
-/**  || [] garantit focus comme un tableau meme si data est undefined
- * [...] crée une copie du tableau data?.focus (ou un tableau vide si data?.focus est undefined).
- */
-
 const Slider = () => {
-  const { data } = useData();
-  const [index, setIndex] = useState(0);
-  const byDateDesc = [...(data?.focus || [])].sort((evtA, evtB) => /**  "?" =optimal chaining. Si data est undefined ou null, l'expression entière renvoie undefined sans générer d'erreur. */
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
-  );  
-
-  /** plus besoin de la fonction nextCard(), car useEffect gère directement l'incrémentation de index avec setInterval */
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setIndex((prevIndex) => (prevIndex < byDateDesc.length - 1 ? prevIndex + 1 : 0));
-    }, 5000);
-    
-    return () => clearInterval(interval); // Nettoyage de l'intervalle
-  }, [byDateDesc.length]);
   
+  const { data } = useData();
+
+  const [index, setIndex] = useState(0);
+
+  const byDateDesc = data?.focus.sort((evtA, evtB) =>
+ // ancien code => new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+ // Correction pour afficher du plus ancien au plus récent
+    new Date(evtA.date) < new Date(evtB.date) ? 1 : -1
+  );
+
+  /** ancien code => 
+   * const nextCard = () => {
+   * setTimeout(
+   * () => setIndex(index < byDateDesc.length ? index + 1 : 0),
+   * 5000
+   * );
+   * };
+  */    
+
+  // Correction pour que le slider affiche la première image une fois arrivé à la dernière sans afficher de page blanche entre les 2
+  const dataLength = data?.focus.length
+
+  const nextCard = () => {
+    setTimeout(
+      // Ajout de -1 après "length"
+      () => setIndex(index < dataLength - 1 ? index + 1 : 0),
+      5000
+    );
+  };
+
+  useEffect(() => {
+    nextCard();
+  });
+
   return (
     <div className="SlideCardList">
       {byDateDesc?.map((event, idx) => (
-        <div key={event.id || idx}>
           <div
+            key={event.title}
             className={`SlideCard SlideCard--${
               index === idx ? "display" : "hide"
             }`}
-          >
+            >
             <img src={event.cover} alt="forum" />
             <div className="SlideCard__descriptionContainer">
               <div className="SlideCard__description">
@@ -42,20 +58,26 @@ const Slider = () => {
               </div>
             </div>
           </div>
-          <div className="SlideCard__paginationContainer">
-            <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
-                <input
-                  key={`${event.id}`}
-                  type="radio"
-                  name="radio-button"
-                  checked={index === radioIdx} /** ici on remplace idx par index car index car idx est l'index du premier map slidecardlist */
-                />
-              ))}
-            </div>
-          </div>
+      // Ajout de la fermeture du premier ".map"
+      ))} 
+      <div className="SlideCard__paginationContainer">
+        <div className="SlideCard__pagination">
+          {byDateDesc?.map((event, radioIdx) => (
+          // {byDateDesc.map((_, radioIdx)
+            <input
+              key={event.title}
+              // key={`${event.id}`} correction event n'a pas d'id
+              type="radio"
+              name="radio-button"
+              checked={index === radioIdx}
+              // checked={idx === radioIdx} correction idx => index 
+              readOnly
+              // console error : Warning: You provided a `checked` prop to a form field without an `onChange` handler. 
+              // This will render a read-only field. If the field should be mutable use `defaultChecked`. Otherwise, set either `onChange` or `readOnly`
+            />
+          ))}
         </div>
-      ))}
+      </div>
     </div>
   );
 };
